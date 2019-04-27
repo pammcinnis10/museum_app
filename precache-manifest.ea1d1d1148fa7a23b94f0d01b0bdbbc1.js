b11f348cd048e9292240bb5e96f0dc90116e77a7
pammurray10 <pammcinnis10@gmail.com>
2019-04-27 14:47:16 -0400

    Updates

---
+++ precache-manifest.ea1d1d1148fa7a23b94f0d01b0bdbbc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0990fe939f5e1ea8ddada9d140aee273",
+    "url": "/museum_app/index.html"
+  },
+  {
+    "revision": "c4e622726282983b45c9",
+    "url": "/museum_app/static/css/main.38d4e9de.chunk.css"
+  },
+  {
+    "revision": "496d96f4d48f7360a369",
+    "url": "/museum_app/static/js/2.50f5a3e5.chunk.js"
+  },
+  {
+    "revision": "c4e622726282983b45c9",
+    "url": "/museum_app/static/js/main.d864cafa.chunk.js"
+  },
+  {
+    "revision": "a6da1ac22736af98864b",
+    "url": "/museum_app/static/js/runtime~main.148c63ef.js"
+  }
+]);
